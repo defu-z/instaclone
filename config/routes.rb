85a5bcd697dded0d50de7d1d920950d9a0919b8f
Defu zhang <zhangdefu686@gmail.com>
2024-05-21 23:17:16 -0400

    INsclone

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+    get '/users/password', to: 'devise/passwords#new'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+
+
+  devise_for :users
+  get 'home/about'
+  resources :posts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+
+  # Defines the root path route ("/")
+  root "posts#index"
+end
